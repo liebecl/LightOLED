8272be5b14d39e4985035d929b4a367ca2d8f83a
liebecl <62412581+liebecl@users.noreply.github.com>
2021-04-09 10:25:44 +0800

    Update OLED.h

--- OLED.h
+++ OLED.h
@@ -1,3 +1,7 @@
 void OLED_Init(int fd);
 void WriteCmd(int fd, unsigned char I2C_Command);
 void WriteData(int fd, unsigned char I2C_Data);
+void OLED_SetPos(int fd,unsigned char x, unsigned char y);
+void Write_DataBuffer(void);
+void OLED_Fill(void);
+void OLED_CLS(void);
